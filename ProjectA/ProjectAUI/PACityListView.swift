99e932f684ca85edb1521447e895da57a71b550d
Bilgehan Isikli <bilgehan.isikli@gmail.com>
2022-07-11 16:56:30 -0700

    [PA] first fetch + swiftUI integration

---
+++ ProjectA/ProjectAUI/PACityListView.swift
@@ -0,0 +1,32 @@
+//
+//  PACityListViewController.swift
+//  ProjectA
+//
+//  Created by Bilgehan ISIKLI on 7/11/22.
+//
+
+import Combine
+import SwiftUI
+
+struct PACityListView: View {
+  @ObservedObject var forecastProvider = PAWeatherProvider()
+  var body: some View {
+    if let error = forecastProvider.error {
+      Text("\(error.localizedDescription)")
+    } else {
+      List {
+        ForEach(forecastProvider.cityForecasts) { cityForecast in
+          Color.orange
+        }
+      }
+    }
+  }
+}
+
+
+struct PACityListView_Previews: PreviewProvider {
+  static var previews: some View {
+    PACityListView()
+  }
+}
+
--- ProjectA/SceneDelegate.swift
+++ ProjectA/SceneDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Bilgehan ISIKLI on 7/11/22.
 //
 
+import SwiftUI
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -15,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     if let windowScene = scene as? UIWindowScene {
       let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UINavigationController(rootViewController: PACityListViewController())
+      window.rootViewController = UIHostingController(rootView: PACityListView())
       self.window = window
       window.makeKeyAndVisible()
   }
